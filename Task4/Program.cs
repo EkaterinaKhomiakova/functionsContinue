a0f8de30b964d57a6f06659920f2c986bc5a2ebe
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-19 16:43:06 +0300

    Код для поиска элементов в массиве

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив из 123 случайных чисел
+// Найдите колличество элементов массива, значение которых лежат в отрезке [10,99]
+// Пример для массива из 5, а не 123 элементов. В своем решении сделайте для 123
+// [5,18,123,6,2]->1
+// [1,2,3,6,2]->0
+// [10,11,12,13,14]->5
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 100);
+}
+
+int ReleaseArray(int[] array)
+{
+  int sum = 0;
+  for (int i = 0; i < array.Length; i++)
+    if (array[i] >= 10 && array[i] <= 99)
+      sum++;
+  return sum;
+}
+
+void PrintArray(int[] array)
+{
+  Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Console.Write($"n=");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+Console.Write(ReleaseArray(array));
